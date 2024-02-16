c7080d68b33ef4874d8ce9e36e28f3d2b67b1850
Daniel Schmitz <danieljfa@gmail.com>
2024-02-16 20:18:02 +0000

    Remove duplicate footer

--- src/views/categories/IndexView.vue
+++ src/views/categories/IndexView.vue
@@ -3,8 +3,5 @@
   <article>
     <header>Categories</header>
     <RouterView />
-    <footer>
-      <router-link to="/categories/create"><button>New</button></router-link>
-    </footer>
   </article>
 </template>
