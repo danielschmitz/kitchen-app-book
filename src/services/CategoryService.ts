93d2bceeed51e19551b1fa9592cf933af244b329
Daniel Schmitz <danieljfa@gmail.com>
2024-02-22 09:01:14 -0300

    delete category

--- src/services/CategoryService.ts
+++ src/services/CategoryService.ts
@@ -8,10 +8,11 @@ export interface Category {
 const URI = '/categories'
 
 export const CategoryService = {
-  getAll: async (): Category[] => (await HttpService.get(URI)).data,
-  get: async (id): Category => (await HttpService.get(URI + '/' + id)).data,
-  create: async (category: Category): Category =>
+  getAll: async (): Promise<Category[]> => (await HttpService.get(URI)).data,
+  get: async (id:any): Promise<Category> => (await HttpService.get(URI + '/' + id)).data,
+  create: async (category: Category): Promise<Category> =>
     (await HttpService.post('/categories', category)).data,
-  update: async (category: Category): Category =>
-    (await HttpService.put('/categories/' + category.id, category)).data
+  update: async (category: Category): Promise<Category> =>
+    (await HttpService.put('/categories/' + category.id, category)).data,
+  delete: async (id:any) => (await HttpService.delete(`${URI}/${id}`)),
 }
--- src/views/categories/EditView.vue
+++ src/views/categories/EditView.vue
@@ -39,9 +39,24 @@ const save = async () => {
     loading.value = false
   }
 }
+
+const remove = async () => {
+  if (confirm("Remove?")) {
+    try {
+      loading.value = true
+      await CategoryService.delete(form.value.id)
+      router.push('/categories')
+    } catch (error) {
+      console.log(error)      
+    } finally {
+      loading.value = false
+    }
+  }
+}
+
 </script>
 <template>
-  <h5>Create Category</h5>
+  <h5>Edit Category</h5>
   <form>
     <input type="hidden" name="id" v-model="form.id" />
     <label>
@@ -49,6 +64,9 @@ const save = async () => {
       <input name="name" type="text" v-model="form.name" placeholder="Category Name" required />
     </label>
   </form>
+  <div style="display: flex;justify-content: center;">
+    <button :aria-busy="loading" :disabled="loading" @click="remove()" class="outline secondary">Delete</button>
+  </div>
   <footer>
     <router-link to="/categories"><button class="outline">Back</button></router-link>
     <button :aria-busy="loading" :disabled="loading" @click="save()">Save</button>
